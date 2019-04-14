a92f17d03e654244ea6412247db1eac62c0c7ec1
Alban Crequy <alban@kinvolk.io>
2019-04-14 19:52:09 +0200

    CLI accepts several cgroups

--- straceback.go
+++ straceback.go
@@ -9,35 +9,37 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		os.Exit(1)
-	}
-	cgroupPath := os.Args[1]
-
 	t, err := straceback.NewTracer()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 
-	id, err := t.AddProg(cgroupPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
+	var ids []uint32
+	for _, cgroupPath := range os.Args[1:] {
+		id, err := t.AddProg(cgroupPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Added cgroup %q as id %v\n", cgroupPath, id)
+		ids = append(ids, id)
 	}
-
-	fmt.Printf("Added prog id %v\n", id)
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, os.Kill)
 
 	<-sig
 	fmt.Printf("Interrupted!\n")
-	_ = t.DumpProgWithQueue(id)
-	err = t.DumpProg(id)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
+
+	for _, id := range ids {
+		_ = t.DumpProgWithQueue(id)
+		err = t.DumpProg(id)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
 	}
+
 	t.Stop()
 }
