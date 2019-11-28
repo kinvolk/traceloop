877f79708f4536085316a8a1b76db0a8f182f673
Alban Crequy <alban@kinvolk.io>
2019-11-28 14:41:09 +0100

    Support cgroup path generated by the systemd driver

--- pkg/procinformer/procinformer.go
+++ pkg/procinformer/procinformer.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"strconv"
+	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -25,7 +26,8 @@ type ProcInformer struct {
 	stopChan         chan struct{}
 	procInformerChan chan ProcInfo
 
-	cgroupRegexp *regexp.Regexp
+	cgroupRegexp1 *regexp.Regexp
+	cgroupRegexp2 *regexp.Regexp
 }
 
 func NewProcInformer(procInformerChan chan ProcInfo) (*ProcInformer, error) {
@@ -40,7 +42,9 @@ func NewProcInformer(procInformerChan chan ProcInfo) (*ProcInformer, error) {
 	// Examples:
 	// 1:name=systemd:/kubepods/burstable/pod533bebda-632d-4a45-9f32-237bcae5b1fc/f2c9e02d3140d5d72f23640b79596dcb043cc3ee818fb61507ff2dfb63dd0211
 	// 1:name=systemd:/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-pod1aecc643_23ea_11e9_beec_06c846f19394.slice/docker-da9977a4f9abe14ab2fa87d3780d92fd615b97cc3107fcd4a851f01857cb8ff8.scope
-	c.cgroupRegexp, _ = regexp.Compile("\n1:name=systemd:.*/kubepods.*[/-]pod([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}).*/([0-9a-f]{64})")
+	// 1:name=systemd:/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-pod5759a1ae_36ca_48a8_b20a_b0b5c8a90fb8.slice/docker-c8b38413c88eefe063b8cd3f01c16be5e3bda9693a19a68a88807baca9feb937.scope
+	c.cgroupRegexp1, _ = regexp.Compile("\n1:name=systemd:.*/kubepods.*[/-]pod([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}).*/([0-9a-f]{64})")
+	c.cgroupRegexp2, _ = regexp.Compile("\n1:name=systemd:.*/kubepods.*[/-]pod([a-f0-9]{8}_[a-f0-9]{4}_[a-f0-9]{4}_[a-f0-9]{4}_[a-f0-9]{12}).*/docker-([0-9a-f]{64})")
 
 	go func() {
 		ticker := time.NewTicker(1000 * time.Millisecond)
@@ -104,11 +108,14 @@ func (p *ProcInformer) update() error {
 			continue
 		}
 
-		matches := p.cgroupRegexp.FindStringSubmatch(string(cgroupContent))
+		matches := p.cgroupRegexp1.FindStringSubmatch(string(cgroupContent))
 		if len(matches) != 3 {
-			continue
+			matches = p.cgroupRegexp2.FindStringSubmatch(string(cgroupContent))
+			if len(matches) != 3 {
+				continue
+			}
 		}
-		podUid := matches[1]
+		podUid := strings.Replace(matches[1], "_", "-", -1)
 		containerID := "docker://" + matches[2]
 		//fmt.Printf("pid %d utsns %d pod %s containerID %s\n", pid, utsns, podUid, containerID)
 
